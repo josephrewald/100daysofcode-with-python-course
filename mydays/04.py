1b341a14385d17f996199d01fe06192e9cbc9df0
Joseph Rewald <joseph.rewald@gmail.com>
2020-10-12 19:12:47 +0200

    COLLECTIONS
    Basically got a fuzzy filter thing working. Gonna fuck shit up a bit by
    trying to make it cooler.

---
+++ mydays/04.py
@@ -0,0 +1,36 @@
+from collections import defaultdict, namedtuple, Counter, deque
+import csv
+import random
+from urllib.request import urlretrieve
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+Snapshot = namedtuple("snapshot", "datetime flow compression T_in T_out pressure")
+
+filelocation = "/home/joseph/projects/100daysofcode-with-python-course/csvfiles/"
+thisfile = filelocation + "Archiv_13.csv"
+day = []
+
+with open(thisfile, newline="", encoding="utf-16") as csvfile:
+    myreader = csv.reader(csvfile, delimiter="\t", quotechar="|")
+    for row in myreader:
+        print(row[1])
+        day.append(Snapshot(row[1], row[3], row[5], row[6], row[7], row[8]))
+
+datetime = []
+flow = []
+compression = []
+T_in = []
+T_out = []
+pressure = []
+for count, enum_row in enumerate(day):
+    datetime.append(day[count].datetime)
+    flow.append(day[count].flow)
+    compression.append(day[count].compression)
+    T_in.append(day[count].T_in)
+    T_out.append(day[count].T_out)
+    pressure.append(day[count].pressure)
+
+with PdfPages(filelocation + "graph.pdf") as export_pdf:
+    plt.plot(datetime, flow, datetime, compression, datetime, T_in, datetime, T_out, datetime, pressure)
+    export_pdf.savefig()
